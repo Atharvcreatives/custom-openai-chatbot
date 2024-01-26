b2b21747ec3cd57ff9e58f2d830e144d5ed3f1c2
Atharv Gour <atharvexe@gmail.com>
2024-01-26 15:37:47 +0530

    working on sidebar

--- frontend/src/pages/Chat.tsx
+++ frontend/src/pages/Chat.tsx
@@ -1,12 +1,21 @@
 //chat page
-import React from 'react'
-import { Box } from '@mui/material'
+import React from "react";
+import { Box } from "@mui/material";
 const chat = () => {
   return (
-    <Box>
-
+    <Box
+      sx={{
+        display: "flex",
+        flex: 1,
+        width: "100%",
+        height: "100%",
+        mt: 3,
+        gap: 3,
+      }}
+    >
+      <Box sx={{ display: { md: "flex", xs: "none", sm: "none" } }}></Box>
     </Box>
-  )
-}
+  );
+};
 
-export default chat
+export default chat;
