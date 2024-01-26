af16e0353c74ad05f38ec972ed17c74cc04c495e
Atharv Gour <atharvexe@gmail.com>
2024-01-26 16:07:23 +0530

    roles defined user and assistant

---
+++ frontend/src/components/chat/ChatItem.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ChatItem = ({
+  content,
+  role,
+}: {
+  content: string;
+  role: "user" | "assistant";
+}) => {
+  return <div>ChatItem</div>;
+};
+
+export default ChatItem;
